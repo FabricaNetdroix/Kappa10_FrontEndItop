30b5b1d61da207e966b0e9b6f4aab8d87ef0094a
r-netdroix <r@netdroix.com.co>
2016-08-04 23:31:59 -0500

    Se agrega modelo de BD y se mejora la funcionalidad de la clase base de acceso a datos.

--- FrontEndItop/Tier.Data/DataParent.cs
+++ FrontEndItop/Tier.Data/DataParent.cs
@@ -15,7 +15,7 @@ namespace Tier.Data
         DatabaseProviderFactory objDatabaseProviderFactory;
         Database objDatabase;
         #endregion
-
+        
         #region [Properties]
         public System.Configuration.ConnectionStringSettings CurrentConnectionString { get { return this.objConnectionString; } }
         public Database CurrentDatabase { get { return this.objDatabase; } }
@@ -47,11 +47,16 @@ namespace Tier.Data
             Update = 3,
             Delete = 4
         }
+
+        public enum DataBaseEntities : byte
+        {
+            BagHours = 1,
+        }
         #endregion
 
         public abstract void AssingParametersValues(MySql.Data.MySqlClient.MySqlCommand cmd, T obj);
 
-        public abstract IEnumerable<T> RetrieveFiltered(T obj);
+        public abstract IList<T> RetrieveFiltered(T obj);
 
         public abstract bool Insert(T obj);
         public abstract bool Insert(T obj, MySql.Data.MySqlClient.MySqlTransaction objTrans);
@@ -61,5 +66,55 @@ namespace Tier.Data
 
         public abstract bool Delete(T obj);
         public abstract bool Delete(T obj, MySql.Data.MySqlClient.MySqlTransaction objTrans);
+    }
+
+    public static class IENumerableExtensions
+    {
+        public static IEnumerable<T> FromDataReader<T>(this IEnumerable<T> list, System.Data.IDataReader dr)
+        {
+            //Instance reflec object from Reflection class coded above
+            Reflection reflec = new Reflection();
+            //Declare one "instance" object of Object type and an object list
+            Object instance;
+            List<Object> lstObj = new List<Object>();
+
+            //dataReader loop
+            while (dr.Read())
+            {
+                //Create an instance of the object needed.
+                //The instance is created by obtaining the object type T of the object
+                //list, which is the object that calls the extension method
+                //Type T is inferred and is instantiated
+                instance = Activator.CreateInstance(list.GetType().GetGenericArguments()[0]);
+
+                // Loop all the fields of each row of dataReader, and through the object
+                // reflector (first step method) fill the object instance with the datareader values
+                foreach (System.Data.DataRow drow in dr.GetSchemaTable().Rows)
+                {
+                    reflec.FillObjectWithProperty(ref instance,
+                            drow.ItemArray[0].ToString(), dr[drow.ItemArray[0].ToString()]);
+                }
+
+                //Add object instance to list
+                lstObj.Add(instance);
+            }
+
+            List<T> lstResult = new List<T>();
+            foreach (Object item in lstObj)
+            {
+                lstResult.Add((T)Convert.ChangeType(item, typeof(T)));
+            }
+
+            return lstResult;
+        }
+    }
+
+    public class Reflection
+    {
+        public void FillObjectWithProperty(ref object objectTo, string propertyName, object propertyValue)
+        {
+            Type tOb2 = objectTo.GetType();
+            tOb2.GetProperty(propertyName).SetValue(objectTo, propertyValue);
+        }
     }
 }
